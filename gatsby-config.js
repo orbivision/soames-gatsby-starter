ffc21e6f4760707278e291b9bf6d6c31d0c2106a
John Eric Fritz <fritz@orbisoftware.com>
2025-06-01 16:57:38 -0500

    Modified to pull Soames theme from npm rather than the local filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,5 +14,30 @@ module.exports = {
         url: process.env.GATSBY_WORDPRESS_URL,
       },
     },
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      // See https://www.gatsbyjs.com/plugins/gatsby-plugin-manifest/?=gatsby-plugin-manifest
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Soames Gatsby Starter WordPress Blog`,
+        short_name: `GatsbyJS & WP`,
+        start_url: `/`,
+        background_color: `#ffffff`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `content/assets/gatsby-icon.png`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `content/assets`,
+      },
+    },
+    // See https://www.gatsbyjs.com/plugins/gatsby-plugin-react-helmet/?=gatsby-plugin-react-helmet
+    `gatsby-plugin-react-helmet`,
   ],
 };
